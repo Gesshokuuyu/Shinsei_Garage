5257a44602effd9eb8b32df6cdabb28c38ff1f51
ForgeSolucoesEstagio <169064248+Gesshokuuyu@users.noreply.github.com>
2025-02-13 16:37:09 -0300

    Update ShinseiLoginUI.vue

--- shinseigarage/src/components/ShinseiLoginUI.vue
+++ shinseigarage/src/components/ShinseiLoginUI.vue
@@ -104,20 +104,7 @@ export default {
     },
     Login(){
       this.validateForm()
-      this.$toast("I'm a toast!", {
-        position: "top-right",
-        timeout: 5000,
-        closeOnClick: true,
-        pauseOnFocusLoss: true,
-        pauseOnHover: true,
-        draggable: true,
-        draggablePercent: 0.6,
-        showCloseButtonOnHover: false,
-        hideProgressBar: true,
-        closeButton: "button",
-        icon: true,
-        rtl: false
-      });
+      
     }
 
   },
